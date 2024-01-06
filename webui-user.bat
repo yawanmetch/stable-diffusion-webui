e3ce46d3152451bd0a57084c24fbf37ae004acd8
yawanmetch <154411428+yawanmetch@users.noreply.github.com>
2024-01-06 13:08:41 +0530

    initilize

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,8 @@
 @echo off
 
-set PYTHON=
+set PYTHON="C:\Users\pixel\AppData\Local\Programs\Python\Python310\python.exe"
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
-
+set COMMANDLINE_ARGS= --xformers
+git pull
 call webui.bat
